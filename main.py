ea366f29f4890ae3df513faf0d9b7793c81a39ad
Ted K <ted.k07@gmail.com>
2025-11-09 13:19:47 +0100

    First commit nr 4

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# print ('Hello World')
-#
-
-print('\nHello in Flight Search Program ver.3.0 \nMomondo')
-# autor programu: Tedy07 ted.k07@gmail.com
+print ('\nHello in Flight Search Program ver.3.0 \nMomondo')
+#autor programu: Tedy07 ted.k07@gmail.com
 # ver produkcyjna Momondo 07.11.2025 r.
-
+# ver.3.1.
 import webbrowser
 import time
 from datetime import datetime
@@ -12,97 +9,58 @@ from datetime import datetime
 menu_options = ('o', 'r', 'x')
 
 while True:
-    print()
-    print('*** MENU ***')
-    print('o = OW One Way Ticket')
-    print('r = RT Return Ticket')
-    print('x = Exit')
-    print()
-    print()
-    user_input = input('Enter an option: ')
+      print()
+      print('*** MENU ***')
+      print('o = OW One Way Ticket')
+      print('r = RT Return Ticket')
+      print('x = Exit')
+      print()
+      print()
+      user_input = input('Enter an option: ')
 
-    if user_input in menu_options:
-        break
-    else:
-        print()
-        print('Option not available! / Opcja niedostępna!')
+      if user_input in menu_options:
+            break
+      else:
+            print()
+            print('Option not available! / Opcja niedostępna!')
 
 # tutaj kończy się menu
 
 if user_input == 'o':
-    print()
-    print('**OW one way ticket**')
-    skad = input("Podaj lotnisko wylotu: ")
-    dokad = input("Podaj lotnisko docelowe: ")
-    data_str = input("Podaj rok miesiac dzień RRRR-MM-DD: ")
+      print ()
+      print ('**OW one way ticket**')
+      skad=input("Podaj lotnisko wylotu: ")
+      dokad=input("Podaj lotnisko docelowe: ")
+      data_str=input("Podaj rok miesiac dzień RRRR-MM-DD: ")
 
-    try:
-        data = datetime.strptime(data_str, "%Y-%m-%d")
-        print(f"Podana data to: {data.strftime('%d-%m-%Y')}")
-    except ValuError:
-        print("Niepoprawny format")
+      try:
+            data=datetime.strptime(data_str, "%Y-%m-%d")
+            print (f"Podana data to: {data.strftime('%d-%m-%Y')}")
+      except ValuError:
+            print("Niepoprawny format")
 
-    lista = ['pl', 'es', 'dk', 'ie', 'de', 'it', 'fr', 'nl', 'no', 'pt', 'ch', 'se', 'cl', 'com.br', 'com.au', 'com',
-             'com.co', 'mx', 'com.pe', 'com.tr', 'co.uk', 'co.za', 'ca', 'cz', 'ee', 'in', 'at', 'ro', 'fi', 'ua']
+      lista = ['pl', 'es', 'dk', 'ie']
+      # dodatkowe państwa 'de', 'it', 'fr', 'nl', 'no', 'pt', 'ch', 'se', 'cl', 'com.br', 'com.au', 'com', 'com.co', 'mx', 'com.pe', 'com.tr', 'co.uk', 'co.za', 'ca', 'cz', 'ee', 'in', 'at', 'ro',  'fi', 'ua'
 
-    wait_time = 3
-    counter = 1
+      wait_time = 3
+      counter=1
 
-    for l in lista:
-        webbrowser.open(
-            "https://www.momondo." + l + "/flight-search/" + skad + "-" + dokad + "/" + data_str + "?ucs=1j6tr23&sort=price_a",
-            new=0)
-        time.sleep(wait_time)
-        wait_time += 2
+      for l in lista:
 
-        print(f"Licznik: {counter}")
-        counter += 1
+          webbrowser.open("https://www.momondo."+ l+ "/flight-search/"+ skad+ "-"+ dokad+ "/"+ data_str+"?ucs=1j6tr23&sort=price_a", new=0)
+          time.sleep(wait_time)
+          wait_time += 2
+          # ('firefox')
 
-    print('\nKoniec.')
+          print(f"Licznik: {counter}")
+          counter+=1
+
+      print ('\nKoniec.')
 # koniec pierwszej opcji wyboru OW
 
-elif user_input == 'r':
-    print()
-    print('*RT return ticket*')
-
-    skad = input("Podaj lotnisko wylotu: ")
-    dokad = input("Podaj lotnisko docelowe: ")
-    data_str = input("Podaj datę wylotu rok miesiac dzień RRRR-MM-DD: ")
-    data_end = input("Podaj datę powrotu rok miesiac dzień RRRR-MM-DD: ")
-
-    try:
-        data = datetime.strptime(data_str, "%Y-%m-%d")
-        print(f"Podana data to: {data.strftime('%d-%m-%Y')}")
-    except ValuError:
-        print("Niepoprawny format")
-
-    try:
-        data = datetime.strptime(data_end, "%Y-%m-%d")
-        print(f"Podana data to: {data.strftime('%d-%m-%Y')}")
-    except ValuError:
-        print("Niepoprawny format")
-
-    lista = ['pl', 'es', 'dk', 'ie', 'de', 'it', 'fr', 'nl', 'no', 'pt', 'ch', 'se', 'cl', 'com.br', 'com.au', 'com',
-             'com.co', 'mx', 'com.pe', 'com.tr', 'co.uk', 'co.za', 'ca', 'cz', 'ee', 'in', 'at', 'ro', 'fi', 'ua']
-
-    wait_time = 3
-    counter = 1
-
-    for l in lista:
-        webbrowser.open(
-            "https://www.momondo." + l + "/flight-search/" + skad + "-" + dokad + "/" + data_str + "/" + data_end + "?ucs=1ngoxby&sort=bestflight_a",
-            new=0)
-        time.sleep(wait_time)
-        wait_time += 2
-
-        print(f"Licznik: {counter}")
-        counter += 1
-
-    print('\nKoniec.')
-# koniec drugiej opcji wyboru RT
 
 elif user_input == 'x':
-    print()
-    print('See you soon')
-    exit()
+            print()
+            print('See you soon')
+            exit()
 
